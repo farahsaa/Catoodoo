9e6a79af35625808a97186e91dd5b7dc9da16115
Sarah Farah <farahsaa@gmail.com>
2019-06-03 18:16:11 -0400

    adding routes for different resource types, fixes #16

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :cats
+  resources :schedules, only: [:new, :create, :show, :destroy]
+  resources :tasks, only: [:index, :show]
+  
+  root to: "home#index"
 end
